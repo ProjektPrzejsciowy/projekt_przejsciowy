4cf11b2d1239731946388ede071269962a4b5f5a
Jachimowski <jachimowski14@gmail.com>
2016-11-26 20:26:21 +0100

    Rozbudowanie projektu o dodatkowy world plugin

--- pp_gazebo_plugin/pp_gazebo_plugin.cc
+++ pp_gazebo_plugin/pp_gazebo_plugin.cc
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2014-2016 Open Source Robotics Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
-*/
 #include <sstream>
 #include <gazebo/msgs/msgs.hh>
 #include "pp_gazebo_plugin.hh"
@@ -21,94 +5,109 @@
 using namespace gazebo;
 
 // Register this plugin with the simulator
-GZ_REGISTER_GUI_PLUGIN(GUIExampleTimeWidget)
+GZ_REGISTER_GUI_PLUGIN(GUIPPPlugin)
 
-/////////////////////////////////////////////////
-GUIExampleTimeWidget::GUIExampleTimeWidget()
-  : GUIPlugin()
+GUIPPPlugin::GUIPPPlugin() : GUIPlugin()
 {
-  QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
-  // Set the frame background and foreground colors
-  this->setStyleSheet(
+   QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+
+   // Set the frame background and foreground colors
+   this->setStyleSheet(
       "QFrame { background-color : rgba(100, 100, 100, 255); color : white; }");
 
-  // Create the main layout
-  QHBoxLayout *mainLayout = new QHBoxLayout;
+   // Create the main layout
+   QHBoxLayout *mainLayout = new QHBoxLayout;
 
-  // Create the frame to hold all the widgets
-  QFrame *mainFrame = new QFrame();
+   // Create the frame to hold all the widgets
+   QFrame *mainFrame = new QFrame();
 
-  // Create the layout that sits inside the frame
-  QHBoxLayout *frameLayout = new QHBoxLayout();
+   // Create the layout that sits inside the frame
+   QHBoxLayout *frameLayout = new QHBoxLayout();
 
-  // // QLabel *label = new QLabel(tr("Sim Time:"));
+   // Add buttons
+   QPushButton *button1 = new QPushButton(tr("Konfiguracja swiata (dodaj kulke)"));
+   QPushButton *button2 = new QPushButton(tr("Zarzadzanie robotami (usun kulke)"));
+   QPushButton *button3 = new QPushButton(tr("Wyniki symulacji"));
 
-  // Create a time label
-  // // QLabel *timeLabel = new QLabel(tr("00:00:00.00"));
+   frameLayout->addWidget(button1);
+   frameLayout->addWidget(button2);
+   frameLayout->addWidget(button3);
 
-  // Add the label to the frame's layout
-  /* frameLayout->addWidget(label);
-  frameLayout->addWidget(timeLabel);
-  connect(this, SIGNAL(SetSimTime(QString)),
-      timeLabel, SLOT(setText(QString)), Qt::QueuedConnection);
-  */
+   connect(button1, SIGNAL(clicked()), this, SLOT(OnButton1()));
+   connect(button2, SIGNAL(clicked()), this, SLOT(OnButton2()));
+   connect(button3, SIGNAL(clicked()), this, SLOT(OnButton3()));
 
-  QPushButton *button1 = new QPushButton(tr("Konfiguracja eksperymentu"));
-  QPushButton *button2 = new QPushButton(tr("Zarzadzanie robotami"));
-  QPushButton *button3 = new QPushButton(tr("Wyniki"));
+   // Add frameLayout to the frame
+   mainFrame->setLayout(frameLayout);
 
-  frameLayout->addWidget(button1);
-  frameLayout->addWidget(button2);
-  frameLayout->addWidget(button3);
+   // Add the frame to the main layout
+   mainLayout->addWidget(mainFrame);
 
-  connect(button1, SIGNAL(clicked()), this, SLOT(OnButton1()));
-  connect(button2, SIGNAL(clicked()), this, SLOT(OnButton2()));
-  connect(button3, SIGNAL(clicked()), this, SLOT(OnButton3()));
+   // Remove margins to reduce space
+   frameLayout->setContentsMargins(4, 4, 4, 4);
+   mainLayout->setContentsMargins(0, 0, 0, 0);
 
-  // Add frameLayout to the frame
-  mainFrame->setLayout(frameLayout);
+   this->setLayout(mainLayout);
 
-  // Add the frame to the main layout
-  mainLayout->addWidget(mainFrame);
+   // Position and resize this widget
+   this->move(0, 0);
+   this->resize(800, 40);
 
-  // Remove margins to reduce space
-  frameLayout->setContentsMargins(8, 8, 8, 8);
-  mainLayout->setContentsMargins(0, 0, 0, 0);
+   // Create a node for transportation
+   this->node = transport::NodePtr(new transport::Node());
+   this->node->Init();
 
-  this->setLayout(mainLayout);
+   // Connect to gazebo topics
+   this->factoryPub = this->node->Advertise<msgs::Factory>("~/factory");
+   this->requestPub = this->node->Advertise<msgs::Request>("~/request");
 
-  // Position and resize this widget
-  this->move(0, 0);
-  this->resize(800, 40);
-
-  // Create a node for transportation
-  // this->node = transport::NodePtr(new transport::Node());
-  // this->node->Init("default");
-  // this->statsSub = this->node->Subscribe("~/world_stats",
-  //     &GUIExampleTimeWidget::OnStats, this);
+   this->counter = 0;
 }
 
-/////////////////////////////////////////////////
-GUIExampleTimeWidget::~GUIExampleTimeWidget()
+GUIPPPlugin::~GUIPPPlugin()
 {
 }
 
-void GUIExampleTimeWidget::OnButton1()
+void GUIPPPlugin::OnButton1()
 {
- QDialog *dialog1 = new QDialog();
- dialog1->setWindowTitle(tr("Konfiguracja eksperymentu")); 
- dialog1->show();
+   msgs::Model model;
+   model.set_name("sphere_" + std::to_string(this->counter++));
+   msgs::Set(model.mutable_pose(), ignition::math::Pose3d(0, 0, 1.5, 0, 0, 0));
+   
+   const double mass = 1.0;
+   const double radius = 0.5;
+   msgs::AddSphereLink(model, mass, radius);
+
+   std::ostringstream newModelStr;
+   newModelStr << "<sdf version='" << SDF_VERSION << "'>"
+               << msgs::ModelToSDF(model)->ToString("")
+               << "</sdf>";
+    
+   // printf("%s", newModelStr.str().c_str());
+
+   // Send the model to the gazebo server
+   msgs::Factory msg;
+   msg.set_sdf(newModelStr.str());
+   this->factoryPub->Publish(msg);
 }
 
-void GUIExampleTimeWidget::OnButton2()
+void GUIPPPlugin::OnButton2()
 {
- QDialog *dialog2 = new QDialog();
- dialog2->show();
+   if (this->counter > 0)
+   {
+      msgs::Request *msg = gazebo::msgs::CreateRequest("entity_delete",
+         "sphere_" + std::to_string(--this->counter));
+      this->requestPub->Publish(*msg);
+   }
 }
 
-void GUIExampleTimeWidget::OnButton3()
+void GUIPPPlugin::OnButton3()
 {
- QDialog *dialog3 = new QDialog();
- dialog3->show();
-}
+   QDialog *dialog1 = new QDialog();
+   dialog1->setWindowTitle(tr("Wyniki symulacji"));
+   
+   QPushButton* tmpButton = new QPushButton("Przykladowy button", dialog1);
+   tmpButton->setGeometry(100, 100, 300, 100);
 
+   dialog1->show();
+}
