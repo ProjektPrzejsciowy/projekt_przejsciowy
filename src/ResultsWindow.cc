86246be134249c10e9990a3dd57a07a66b3e8cfe
jedrek1993 <jedrek1993@gmail.com>
2017-01-17 11:26:18 +0000

    poprawiono wykres pozycji robota

--- src/ResultsWindow.cc
+++ src/ResultsWindow.cc
@@ -19,25 +19,29 @@ ResultsWindow::ResultsWindow() : QDialog()
 	
 	//plot->addGraph();	
 	//plot->graph(0)->setPen(QPen(QColor(40, 110, 255)));
-	plot->xAxis->setRange(-5, 5);
-	plot->yAxis->setRange(-5, 5);
+	plot->xAxis->setRange(-6, 6);
+	plot->yAxis->setRange(-4, 4);
 	plot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
 	plot->axisRect()->setupFullAxesBox();
 	plot->rescaleAxes();
 
 	Spiral1 = new QCPCurve(this->plot->xAxis, this->plot->yAxis);
 	connect(&dataTimer, SIGNAL(timeout()), this, SLOT(realtimeDataSlot()));
- 	dataTimer.start(0); // Interval 0 means to refresh as fast as possible
+ 	dataTimer.start(500); // Interval 0 means to refresh as fast as possible
 	
 
 
 	QPushButton *buttonPoseReg1 = new QPushButton("Rejestruj pozycje", tab1);
-	buttonPoseReg1->setGeometry(QRect(QPoint(20, 420),QSize(200, 30)));
+	buttonPoseReg1->setGeometry(QRect(QPoint(10, 420),QSize(180, 30)));
 	connect(buttonPoseReg1, SIGNAL(clicked()), this, SLOT(OnButtonPoseReg1()));
 	
-	QPushButton *buttonSave1 = new QPushButton("Zapisz do pliku", tab1);
-	buttonSave1->setGeometry(QRect(QPoint(300, 420),QSize(200, 30)));
+	QPushButton *buttonSave1 = new QPushButton("Pauza", tab1);
+	buttonSave1->setGeometry(QRect(QPoint(200, 420),QSize(180, 30)));
 	connect(buttonSave1, SIGNAL(clicked()), this, SLOT(OnButtonSave1()));
+
+	QPushButton *buttonStop = new QPushButton("Stop", tab1);
+	buttonStop->setGeometry(QRect(QPoint(400, 420),QSize(100, 30)));
+	connect(buttonStop, SIGNAL(clicked()), this, SLOT(OnButtonStop()));
 
 
 	this->adjustSize();
@@ -55,9 +59,6 @@ ResultsWindow::ResultsWindow() : QDialog()
 
 void ResultsWindow::Received(const boost::shared_ptr<const msgs::Quaternion> &msg)
 {
-	cout<<"X: "<< double(msg->x()) <<" Y: "<< double(msg->y()) <<endl;
-	//this->plot->graph(0)->addData(double(msg->x()), double(msg->y()));
-	//this->plot->replot();
 	x1.push_back(double(msg->x()));
 	y1.push_back(double(msg->y()));
 	
@@ -76,18 +77,27 @@ void ResultsWindow::OnButtonSave1()
 	this->publisher->Publish(MyMsg);
 }
 
+void ResultsWindow::OnButtonStop()
+{
+	msgs::Int MyMsg;
+	MyMsg.set_data(511);
+	this->publisher->Publish(MyMsg);
+	this->x1.clear();
+	this->y1.clear();
+}
+
 
 void ResultsWindow::realtimeDataSlot()
 {
-  static QTime time(QTime::currentTime());
-  double key = time.elapsed()/1000.0; // time elapsed since start of demo, in seconds
-  static double lastPointKey = 0;
-  if (key-lastPointKey > 0.002) // at most add point every 2 ms
-  {
+  //static QTime time(QTime::currentTime());
+  //double key = time.elapsed()/1000.0; // time elapsed since start of demo, in seconds
+  //static double lastPointKey = 0;
+  //if (key-lastPointKey > 0.002) // at most add point every 2 ms
+  //{
    	this->Spiral1->setData(this->x1, this->y1);
 	this->plot->replot();
-    lastPointKey = key;
-  }
+  //  lastPointKey = key;
+  //}
 
 }
 
